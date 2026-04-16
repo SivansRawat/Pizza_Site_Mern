14b531b028f6ee20bf557c62ac4dc70ce334e9b7
Sivans Rawat <rawatsivans@gmail.com>
2026-04-16 23:30:20 +0530

    Fix ESLint errors for Vercel build

--- client/src/actions/orderActions.js
+++ client/src/actions/orderActions.js
@@ -73,7 +73,7 @@ export const getUserOrders = () => async (dispatch, getState) => {
 
 export const getAllOrders=()=>async (dispatch,getState)=>{
 
-  const currentUser = getState().loginUserReducer.currentUser
+  // const currentUser = getState().loginUserReducer.currentUser
   dispatch({type:'GET_ALLORDERS_REQUEST'})
   
   try {
--- client/src/screens/Adminscreen.js
+++ client/src/screens/Adminscreen.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import  { useEffect } from "react";
+import { useSelector} from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import { Link } from "react-router-dom";
 
