cc78f0053eade3462c1c10c2170dc4c062e26b9c
Sivans Rawat <rawatsivans@gmail.com>
2026-04-17 05:47:49 +0530

    Fix Render deployment - remove client build serving

--- server.js
+++ server.js
@@ -26,18 +26,20 @@ app.use('/api/users/' , userRoute)
 app.use('/api/orders/' , ordersRoute)
 
 
-if(process.env.NODE_ENV ==='production')
-{
-    app.use('/' , express.static('client/build'))
+// if(process.env.NODE_ENV ==='production')
+// {
+//     app.use('/' , express.static('client/build'))
 
-    app.get('*' , (req , res)=>{
+//     app.get('*' , (req , res)=>{
 
-        res.sendFile(path.resolve(__dirname  , 'client/build/index.html'))
+//         res.sendFile(path.resolve(__dirname  , 'client/build/index.html'))
 
-    })
-}
+//     })
+// }
 
-
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
 
 
